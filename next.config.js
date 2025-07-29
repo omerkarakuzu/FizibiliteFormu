baf638d5fc19c66617ccdf00799e5511cae2f896
Ömer Karakuzu <omerkarakuzu4@gmail.com>
2025-07-29 11:25:57 +0300

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const isGithubPages = process.env.NODE_ENV === "production";
-
-module.exports = {
-  output: "export", // next build ile otomatik static export yapılır
-  basePath: isGithubPages ? "/FizibiliteFormu" : "",
-  assetPrefix: isGithubPages ? "/FizibiliteFormu/" : "",
-  images: {
-    unoptimized: true, // GitHub Pages için zorunlu (çünkü Image Optimization desteklenmiyor)
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/FizibiliteFormu",
+  assetPrefix: "/FizibiliteFormu",
 };
+
+module.exports = nextConfig;
