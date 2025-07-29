e96f7de2fd59cc25ca16e6074570fcc92f6712d7
Ömer Karakuzu <omerkarakuzu4@gmail.com>
2025-07-29 07:22:30 +0300

    backup commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     unoptimized: true,
   },
-  basePath: "/FizibiliteFormu",
-  assetPrefix: "/FizibiliteFormu/",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
